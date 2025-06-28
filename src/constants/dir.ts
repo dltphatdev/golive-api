aa3d512ba592ae0ead51160077b89551dd1ed4de
Thanh Phat <dltphat301@gmail.com>
2025-06-28 09:20:47 +0000

    update src code

--- src/constants/dir.ts
+++ src/constants/dir.ts
@@ -1,5 +1,5 @@
 import { isProduction } from '@/constants/config'
 import path from 'path'
 
-export const UPLOAD_IMAGE_DIR = path.resolve(isProduction ? '/home/goliveprod/uploads/image' : 'uploads/image')
-export const UPLOAD_IMAGES_DIR = path.resolve(isProduction ? '/home/goliveprod/uploads/images' : 'uploads/images')
+export const UPLOAD_IMAGE_DIR = path.resolve(isProduction ? '/home/vmadmin/uploads/image' : 'uploads/image')
+export const UPLOAD_IMAGES_DIR = path.resolve(isProduction ? '/home/vmadmin/uploads/images' : 'uploads/images')
