4d4f066759e42873bea44a2da36b73bd0703ddc6
Thanh Phat <dltphat301@gmail.com>
2025-06-29 04:07:05 +0000

    update api 29.6.2025

--- src/utils/crypto.ts
+++ src/utils/crypto.ts
@@ -1,10 +1,12 @@
 import { CONFIG_ENV } from '@/constants/config'
-import { createHash } from 'node:crypto'
+import bcrypt from 'bcrypt'
 
-function sha256(content: string) {
-  return createHash('sha256').update(content).digest('hex')
+// Mã hóa password
+export async function hashPassword(password: string) {
+  return await bcrypt.hash(password, 10) // cost = 10 là mức tiêu chuẩn
 }
 
-export function hashPassword(password: string) {
-  return sha256(password + CONFIG_ENV.PASSWORD_SECRET)
+// So sánh khi login
+export async function comparePassword(input: string, hash: string) {
+  return await bcrypt.compare(input, hash)
 }
